ed787d08276fbeae2ab3a9bb6e8bd4d8d5576ae6
angeloren92 <angeloren92@gmail.com>
2026-02-17 14:26:44 +0100

    add array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 console.log('test');
 
+const bikes = [
+    {
+        nome: 'bike-1',
+        peso: 12
+    },
+    {
+        nome: 'bike-2',
+        peso: 8
+    },
+    {
+        nome: 'bike-3',
+        peso: 15
+    },
+    {
+        nome: 'bike-4',
+        peso: 5
+    },
+    {
+        nome: 'bike-5',
+        peso: 9
+    }
+]
+
+console.log(bikes)
